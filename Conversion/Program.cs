a2ea6bdfbd2c18aaf078e9b1d9b5fa1d5e0e2c16
Akshu <aksharadpachu2001@gmail.com>
2023-10-26 13:21:16 +0530

    created conversion

---
+++ Conversion/Program.cs
@@ -0,0 +1,79 @@
+﻿//using System.Globalization;
+
+//EXPLICIT CONVERSION :means converting larger datatypes into smaller datatypes
+//How to convert float to int
+/*namespace Datatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 3.14f;
+            int a = (int)num1;
+            Console.WriteLine(a);
+            // while converting float to int the decimal values are not printed in the output only thr integer values is printed
+        }
+    }
+}*/
+// IMPLICIT CONVERSION:means converting smaller dattypes to larger datatypes
+/*namespace Datatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 23;
+            long num1 = a;
+            // int has only 32 bit storage so it will display as Int32
+            // long has 64 bit storage so it will display as Int64
+            // we are using "GetType()" for finding the datatype 
+            Console.WriteLine(num1.GetType());
+            Console.WriteLine(a.GetType());
+
+        }
+    }
+}*/
+//TYPE CONVERSION : means changing  the value of the datatype
+// Converting "int, float, double " to "string" 
+/*namespace Datatypes
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int a = 50; // float,double
+            Console.WriteLine(a);
+            Console.WriteLine(a.GetType());
+            // we cannot directly convert int to string so we use a keyword .ToString()
+            string value = a.ToString();
+            Console.WriteLine(value);
+            Console.WriteLine(value.GetType());
+
+           
+        }
+    }
+}*/
+// HOW TO GET INPUT FROM THE USER
+using System.Reflection.Metadata.Ecma335;
+using System.Transactions;
+
+namespace Datatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//in this case we are getting the input from the user 
+            Console.WriteLine("Enter a Two Numbers");
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+// to add two numbers we have to change the intput from string to integer
+// to change a string to int use the keyword of Int32.Parse()
+            int num1 = Int32.Parse(a); 
+            int num2 = Int32.Parse(b);
+            int c = num1 + num2;
+
+            Console.WriteLine(c);
+        }
+    }
+}
--- Variables/Program.cs
+++ Variables/Program.cs
@@ -10,7 +10,7 @@
 //2] REFERENCE TYPE : means uses the indexing method & it helps to refer the value in the variable name 
 //i) String
 //ii) Array & Alass
-/*namespace Datatypes
+/*amespace Datatypes
 {
     class Program
     {
@@ -22,9 +22,9 @@
             Console.WriteLine(a);
         }
     }
-}*/
+}
 //how to add two values 
-/*namespace Datatypes
+namespace Datatypes
 {
     class Program
     {
@@ -36,9 +36,9 @@
             Console.WriteLine(c);
         }
     }
-}*/
+}
 // FLOAT
-/*namespace Datatypes
+namespace Datatypes
 {
     class Program
     {
@@ -50,7 +50,7 @@
             Console.WriteLine(num3);
         }
     }
-}*/
+}
 //LONG
 // We use long datatype for identifying the size of the datatypes
 // for int = 4 bytes
@@ -60,7 +60,7 @@
 // integer datatype only can store 4bytes of the data  but if we use "long" we can store upto 8 bytes of the data
 // we are changing "int" to "long" becoz "int" has low storage capacity
 
-/*namespace Datatypes
+namespace Datatypes
 {
     class Program
     {
@@ -77,7 +77,7 @@
 // whta is the difference  between double & float?
 // float only gives the "approximate or limited values" as it has only limited memory storage capacity of 4 bytes.
 // double can give larger number values as it has the memory storage capacity of 8 bytes.
-/*namespace Datatypes
+namespace Datatypes
 {
     class Program
     {
@@ -91,4 +91,4 @@
 
 
     }
-}*/
+}
